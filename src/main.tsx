a8a2e40155eeab9b43d6a3c360a3c025822e5b07
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-04-23 16:34:43 +0200

    :tada: Init project (bis)

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
+import './reset.sass'
+import './pages/page.sass'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
