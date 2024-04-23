8e3aa4aeca7a38f9ce6005bebcf35f5461f6147e
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-04-23 16:39:12 +0200

    :rocket: Setup routing system with react-router

---
+++ src/router/index.tsx
@@ -0,0 +1,30 @@
+import {createBrowserRouter, RouteObject} from 'react-router-dom'
+import Home from '../pages/home'
+import NotFound from '../pages/notFound'
+import Layout from "../layouts/Layout.tsx"
+import RentViewer from '../pages/rentViewer'
+import About from "../pages/about";
+
+export const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      },
+      {
+        path: 'about',
+        element: <About />
+      },
+      {
+        path: 'rent/:id',
+        element: <RentViewer />
+      }
+    ]
+  }
+]
+
+export const router = createBrowserRouter(routes)
