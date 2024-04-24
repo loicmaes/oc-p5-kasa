d665faf6136b7dd8e92757ac8c257a170885c35a
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-04-24 09:15:48 +0200

    :rocket: Setup 404 - not found page with responsive behavior

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,3 +1,18 @@
+import TopNavigation from "../../components/navigation/topNavigation/TopNavigation.tsx";
+import BottomCard from "../../components/navigation/bottomCard/BottomCard.tsx";
+import {NavLink} from "react-router-dom";
+import './style.sass'
+
 export default function Index () {
-  return <main className="page"></main>
+  return <>
+    <TopNavigation />
+
+    <main className="page nf">
+      <h1 className="nf--title">404</h1>
+      <h2 className="nf--subtitle">Oups! La page que vous demandez n'existe pas.</h2>
+      <NavLink to="/" className="nf--back">Retourner sur la page d’accueil</NavLink>
+    </main>
+
+    <BottomCard />
+  </>
 }
