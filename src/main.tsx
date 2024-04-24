9e99c61762a524939003a793bd1f0529b5ef88a5
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-04-24 09:41:35 +0200

    :ambulance: Trying something to fix white screen on deploy (gh-pages)

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
+import {BrowserRouter, RouterProvider} from 'react-router-dom'
 import { router } from './router'
 import './reset.sass'
 import './pages/page.sass'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <BrowserRouter basename="https://loicmaes.github.io/oc-p5-kasa">
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
